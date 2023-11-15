54c12888e2cc0fefee93884d3b748c774251fa12
森 航 <mori-wataru@rt.local>
2023-11-15 17:09:45 +0900

    commit

---
+++ Rail_Wear_Plot/module/Load_setting.py
@@ -0,0 +1,16 @@
+#.jsoncファイル設定読み込み関数
+#.jsonは//でコメントが書けるがPythonではエラーを吐く、この関数で読み飛ばせるようにする
+
+import json
+import re
+from pathlib import Path
+
+mod_dir=Path(__file__).resolve().parent
+#今実行している.pyコードのディレクトリの一つ親のディレクトリを返す。要はmoduleフォルダ
+
+def load_json(filename: str, encoding: str = 'utf-8'):
+    with open(mod_dir.joinpath(filename),'r',encoding=encoding)as f:
+        text = f.read()                                              # ファイルの中身を文字列として取得
+    text_without_comment = re.sub(r'/\*[\s\S]*?\*/|//.*', '', text)  # 正規表現を使ってコメントを削除
+    json_obj = json.loads(text_without_comment)                      # 文字列をjson形式として処理
+    return json_obj
---
+++ Rail_Wear_Plot/module/csv_file_load.py
@@ -0,0 +1,3 @@
+import Load_setting 
+testloadsetting = Load_setting.load_json('settings.json')
+a="as"
